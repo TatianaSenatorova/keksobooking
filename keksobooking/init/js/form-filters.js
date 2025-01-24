cee02c8908c19566c63ad9380690ed8566d870f8
TatianaSenatorova <108460915+TatianaSenatorova@users.noreply.github.com>
2025-01-24 11:30:43 +0300

    fix: корректирует фильтрацию

--- keksobooking/init/js/form-filters.js
+++ keksobooking/init/js/form-filters.js
@@ -10,46 +10,34 @@ import { PriceRange, DEFAULT_SELECT_VALUE } from './constants.js';
 let appartments = [];
 let filteredAppartments = [];
 
-const filterType = (appartment) =>
-  typeSelect.value === appartment.offer.type ||
-  typeSelect.value === DEFAULT_SELECT_VALUE;
-
-const filterPrice = (appartment) =>
-  ((appartment.offer.price > PriceRange[priceSelect.value].min &&
-    appartment.offer.price < PriceRange[priceSelect.value].max) ||   priceSelect.value === DEFAULT_SELECT_VALUE);
-
-const filterRooms = (appartment) =>
-  parseInt(roomSelect.value, 10) === appartment.offer.rooms ||
-  roomSelect.value === DEFAULT_SELECT_VALUE;
-
-const filterFeatures = (appartment) => {
-  const checkBoxesArray = [];
-  Array.from(featureCheckBoxes).map((checkbox) => {
-    if (checkbox.checked === true) {
-      checkBoxesArray.push(checkbox.value);
+const FiltersFunctions = {
+  TYPE: (appartment) =>  typeSelect.value === appartment.offer.type ||
+  typeSelect.value === DEFAULT_SELECT_VALUE,
+  PRICE: (appartment) => ((appartment.offer.price > PriceRange[priceSelect.value].min && appartment.offer.price < PriceRange[priceSelect.value].max) ||   priceSelect.value === DEFAULT_SELECT_VALUE),
+  ROOMS: (appartment) => parseInt(roomSelect.value, 10) === appartment.offer.rooms || roomSelect.value === DEFAULT_SELECT_VALUE,
+  FEATURES: (appartment) => {
+    const featuresChosen = (Array.from(featureCheckBoxes).
+      filter((checkbox) => checkbox.checked)).
+      map((checkbox) => checkbox.value);
+    if(featuresChosen.length && appartment.offer.features){
+      return featuresChosen.every((feature) =>
+        appartment.offer.features.includes(feature));
     }
-  });
-  if(appartment.offer.features) {
-    return checkBoxesArray.every((checkbox) =>
-      appartment.offer.features.includes(checkbox));
-  }
-  return appartment;
+  },
 };
 
 const getFilteredAppartments = () => {
-  console.log(appartments);
   filteredAppartments = [];
   for (let i = 0; i < appartments.length; i++) {
     if (
-      filterType(appartments[i]) &&
-      filterRooms(appartments[i]) &&
-      filterPrice(appartments[i]) &&
-      filterFeatures(appartments[i])
+      FiltersFunctions.TYPE((appartments[i])) &&
+      FiltersFunctions.PRICE((appartments[i])) &&
+      FiltersFunctions.ROOMS((appartments[i])) &&
+      FiltersFunctions.FEATURES(appartments[i])
     ) {
       filteredAppartments.push(appartments[i]);
     }
   }
-  console.log(filteredAppartments);
 };
 
 const setFilters = (appartmentsData) => {
